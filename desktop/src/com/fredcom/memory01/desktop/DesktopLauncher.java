5c26e94b5d24dc3ac0c2d8681a63b0c22d97ed14
fredeaux <51965679+fredeaux@users.noreply.github.com>
2022-03-23 15:09:24 +0100

    Initial commit

---
+++ desktop/src/com/fredcom/memory01/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fredcom.memory01.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fredcom.memory01.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
